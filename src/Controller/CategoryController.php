1492fc026e7de1863b4e0b18e59e3061da312919
fab-web-dev <97179540+fab-web-dev@users.noreply.github.com>
2022-04-20 12:33:07 +0200

    Affichage photos dans home; Création Controller Category avec slug et param converter

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category/{slug}', name: 'app_category')]
+    public function index(category $category): Response
+    {
+        // dd($category);
+
+        return $this->render('category/index.html.twig', [
+            'category' => $category,
+        ]);
+    }
+}
