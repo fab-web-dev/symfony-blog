af16ccc5437e8db422176ac6aedf0ac2c5860c1d
fab-web-dev <97179540+fab-web-dev@users.noreply.github.com>
2022-05-09 17:06:56 +0200

    mise en place API

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
